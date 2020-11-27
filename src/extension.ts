71da905e4d0d15193b1a289b0885aac49ba2c1a2
Andrew McGoveran <andrew.mcgoveran@shopify.com>
2020-11-27 18:10:08 -0500

    Added basic generateComponent command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,29 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {commands, window, ExtensionContext} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "react-component-generator" is now active!');
+export function activate(context: ExtensionContext) {
+	console.log('React component generator is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('react-component-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const generateComponent = commands.registerCommand('react-component-generator.generateComponent', async () => {
+		// Code in here is executed every time the command is executed
+		const componentName = await window.showInputBox();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from React Component Generator!');
+		if (componentName) {
+			window.showInformationMessage(`Your component will be called ${componentName}.`);
+			console.log({componentName})
+		} else {
+			console.error('No component name passed')
+		}
+
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(generateComponent);
 }
 
 // this method is called when your extension is deactivated
