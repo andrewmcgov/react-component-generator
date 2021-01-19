9fdb8a7e49f67b04e9920376cb38ac627ee029a9
Andrew McGoveran <andrew.mcgoveran@shopify.com>
2021-01-18 21:20:11 -0500

    Add settings for test file, styles file, index file, and default export

--- src/templates/exportLineTemplate.ts
+++ src/templates/exportLineTemplate.ts
@@ -1,3 +1,15 @@
-export function exportLineTemplate(componentName: string) {
-  return `export {${componentName}} from './${componentName}';\n`;
+export function exportLineTemplate(
+  componentName: string,
+  defaultExport: boolean,
+  componentsFolderIndex?: boolean
+) {
+  if (componentsFolderIndex) {
+    return defaultExport
+      ? `export {default as ${componentName}} from './${componentName}';\n`
+      : `export {${componentName}} from './${componentName}';\n`;
+  }
+
+  return defaultExport
+    ? `export {default} from './${componentName}';\n`
+    : `export {${componentName}} from './${componentName}';\n`;
 }
