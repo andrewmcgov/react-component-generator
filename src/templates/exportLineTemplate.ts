0387191f1a2d35024b9c918d0a1af0e6ef60872b
Andrew McGoveran <andrew.mcgoveran@shopify.com>
2021-04-08 13:50:45 -0400

    Use export * in ComponentName/index.ts

--- src/templates/exportLineTemplate.ts
+++ src/templates/exportLineTemplate.ts
@@ -2,8 +2,7 @@ export function exportLineTemplate(
   componentName: string,
   componentsFolderIndex?: boolean
 ) {
-  // These return the same thing right now, leaving them in because we will soon switch this the second one to export * form './ComponentName'
   return componentsFolderIndex
     ? `export {${componentName}} from './${componentName}';\n`
-    : `export {${componentName}} from './${componentName}';\n`;
+    : `export * from './${componentName}';\n`;
 }
