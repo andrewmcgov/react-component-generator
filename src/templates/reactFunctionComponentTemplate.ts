a87b0418a1152a1d3e764183d28a117f45ac5112
alankalb <alan.kalbfleisch@shopify.com>
2021-05-14 17:17:52 -0700

    Add stylesLanguage as argument for reactFunctionComponentTemplate

--- src/templates/reactFunctionComponentTemplate.ts
+++ src/templates/reactFunctionComponentTemplate.ts
@@ -1,8 +1,13 @@
-export function reactFunctionComponentTemplate(componentName: string) {
+import { StyleLanguage } from '../types';
+
+export function reactFunctionComponentTemplate(
+  componentName: string,
+  stylesLanguage: StyleLanguage = StyleLanguage.scss
+) {
   return `
 import React from 'react';
 
-import styles from './${componentName}.scss';
+import styles from './${componentName}.${stylesLanguage}';
 
 export interface ${componentName}Props {
   prop?: string;
